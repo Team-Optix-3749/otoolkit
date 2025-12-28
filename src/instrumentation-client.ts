b4639a9fa5e5a27cb092373d24558931d43cc7e4
SlushEE0 <neeladem2009@gmail.com>
2025-12-27 23:19:41 -0800

    testing network usage of reverse proxying posthog vs not.

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -6,8 +6,16 @@ const posthogKey = assertEnv(
   process.env.NEXT_PUBLIC_POSTHOG_KEY
 );
 
+// no reverse proxy for PostHog
+// posthog.init(posthogKey, {
+//   // api_host: "/ph",
+//   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+//   defaults: "2025-11-30"
+// });
+
+// yes reverse proxy for PostHog
 posthog.init(posthogKey, {
   api_host: "/ph",
-  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.posthog.com",
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
   defaults: "2025-11-30"
 });
